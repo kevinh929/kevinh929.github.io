08a5a8dbf6e6f81d5374f67983d46a87f8018a7f
Broncodolphin <kevinhoagland929@u.boisestate.edu>
2025-04-17 04:11:40 -0600

    Testing Hamburgs

---
+++ menu.js
@@ -0,0 +1,8 @@
+const menuIcon = document.querySelector('.menuIcon');
+
+const offScreen = document.querySelector('.menuOffPage');
+
+menuIcon.addEventListener('click', () => {
+    offScreen.classList.toggle('active');
+    menuIcon.classList.toggle('active');
+});

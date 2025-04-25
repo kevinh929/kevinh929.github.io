700e091d383baff9f3a835bd57157bb7045b9041
Broncodolphin <kevinhoagland929@u.boisestate.edu>
2025-04-25 16:12:41 -0600

    switched to auto slide

--- image.js
+++ image.js
@@ -1,21 +1,15 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = 0;
+autoShowSlides();
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function showSlides(n) {
+function autoShowSlides() {
     let slides = document.getElementsByClassName("TSBGallery");
-    let dots = document.getElementsByClassName("numberdescr");
-    if (n > slides.length) { slideIndex = 1; }
-    if (n < 1) { slideIndex = slides.length; }
     for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+        slides[i].classList.remove("active");
     }
-    for (let i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
     }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+    slides[slideIndex - 1].classList.add("active");
+    setTimeout(autoShowSlides, 3000);
 }

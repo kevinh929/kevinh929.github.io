2dcc3af34b3255439e588ff23064b8dad339e82a
Broncodolphin <kevinhoagland929@u.boisestate.edu>
2025-04-25 15:51:04 -0600

    more path fixes cause im a dumbass

--- image.js
+++ image.js
@@ -21,6 +21,5 @@ function showSlides(n) {
     slides[i].style.display = "none";
   }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }

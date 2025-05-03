f9d7429d808c928f7de7ebc4402f0461288a30f9
Broncodolphin <kevinhoagland929@u.boisestate.edu>
2025-05-03 04:19:34 -0600

    Changed Speed of Parallax

--- AnimBG.js
+++ AnimBG.js
@@ -1,4 +1,4 @@
 document.addEventListener('scroll', () => {
     const scrollPosition = window.scrollY;
-    document.body.style.backgroundPositionY = `${scrollPosition * 0.5}px`; // Adjust the multiplier for parallax speed
+    document.body.style.backgroundPositionY = `${scrollPosition * 0.25}px`;
 });
